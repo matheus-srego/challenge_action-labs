d1826e5d7fe3ae3f35d4ac5da3daf2a82d0e3aca
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-25 17:40:02 -0300

    feat(dto): create DTO for field error response

---
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/dto/FieldErrorsResponseDTO.java
@@ -0,0 +1,32 @@
+package br.com.actionlabs.carboncalc.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class FieldErrorsResponseDTO {
+
+    private int status;
+
+    private String error;
+
+    private String message;
+
+    private LocalDateTime timestamp;
+
+    private List<FieldError> errors;
+
+    @Data
+    public static class FieldError {
+        private String field;
+        private String message;
+
+        public FieldError(String field, String message) {
+            this.field = field;
+            this.message = message;
+        }
+    }
+
+}
