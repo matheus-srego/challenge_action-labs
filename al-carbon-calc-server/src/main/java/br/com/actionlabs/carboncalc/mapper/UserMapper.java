90c56e3ca425c96ad5302b58e23017b5330c5258
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-25 20:03:57 -0300

    refactor(mapper): convert class for interface using library

--- al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/mapper/UserMapper.java
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/mapper/UserMapper.java
@@ -3,24 +3,13 @@ package br.com.actionlabs.carboncalc.mapper;
 import br.com.actionlabs.carboncalc.dto.StartCalcRequestDTO;
 import br.com.actionlabs.carboncalc.dto.StartCalcResponseDTO;
 import br.com.actionlabs.carboncalc.model.UserEmissionFactor;
+import org.mapstruct.Mapper;
 
-public class UserMapper {
+@Mapper(componentModel = "spring")
+public interface UserMapper {
 
-    public static UserEmissionFactor startCalcRequestDTOToUser(StartCalcRequestDTO dto) {
-        final UserEmissionFactor user = new UserEmissionFactor();
-        user.setName(dto.getName());
-        user.setEmail(dto.getEmail());
-        user.setUf(dto.getUf());
-        user.setPhoneNumber(dto.getPhoneNumber());
+    UserEmissionFactor toModel(StartCalcRequestDTO dto);
 
-        return user;
-    }
-
-    public static StartCalcResponseDTO userToStartCalcResponseDTO(UserEmissionFactor user) {
-        final StartCalcResponseDTO responseDTO = new StartCalcResponseDTO();
-        responseDTO.setId(user.getId());
-
-        return responseDTO;
-    }
+    StartCalcResponseDTO toResponseDTO(UserEmissionFactor user);
 
 }
