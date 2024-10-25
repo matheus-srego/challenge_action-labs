3369c29281ffb850946a1a9efea1ef53fd5008d5
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-25 17:44:38 -0300

    feat(constant): create class to store messages

---
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/utils/CONSTANTS.java
@@ -0,0 +1,15 @@
+package br.com.actionlabs.carboncalc.utils;
+
+public class CONSTANTS {
+
+    public static final String NAME_NOT_BLANK = "The name field cannot be blank.";
+
+    public static final String EMAIL_NOT_BLANK = "The email field cannot be blank.";
+
+    public static final String UF_NOT_BLANK = "The UF field cannot be blank.";
+
+    public static final String PHONE_NOT_BLANK = "The phone number field cannot be blank.";
+
+    public static final String EMAIL_INVALID = "Invalid e-mail format.";
+
+}
