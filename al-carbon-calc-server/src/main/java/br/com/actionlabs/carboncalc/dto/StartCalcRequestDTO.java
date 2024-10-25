a2cd4b8c2906f443b2f6b928b87409bab7f0dbf5
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-25 17:28:49 -0300

    feat(dto): add annotations 'Email' and 'NotBlank' for fields validations

--- al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/dto/StartCalcRequestDTO.java
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/dto/StartCalcRequestDTO.java
@@ -1,11 +1,24 @@
 package br.com.actionlabs.carboncalc.dto;
 
+import br.com.actionlabs.carboncalc.utils.CONSTANTS;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class StartCalcRequestDTO {
+
+  @NotBlank(message = CONSTANTS.NAME_NOT_BLANK)
   private String name;
+
+  @NotBlank(message = CONSTANTS.EMAIL_NOT_BLANK)
+  @Email(message = CONSTANTS.EMAIL_INVALID)
   private String email;
+
+  @NotBlank(message = CONSTANTS.UF_NOT_BLANK)
   private String uf;
+
+  @NotBlank(message = CONSTANTS.PHONE_NOT_BLANK)
   private String phoneNumber;
+
 }
