70961a785482ae2cd9bc3fa2d1d2c38e33bfe693
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-28 19:59:24 -0300

    feat(repository): create query for find solid waste by UF

--- al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/repository/SolidWasteEmissionFactorRepository.java
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/repository/SolidWasteEmissionFactorRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SolidWasteEmissionFactorRepository extends MongoRepository<SolidWasteEmissionFactor, String> {
 
+    SolidWasteEmissionFactor findByUf(String uf);
+
 }
