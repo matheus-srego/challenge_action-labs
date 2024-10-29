a2c328b2613d9de8ce65adfa962fa8d334979062
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-28 23:19:52 -0300

    feat(dto): add annotations 'DecimalMin' and 'DecimalMax' for size validation

--- al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/dto/UpdateCalcInfoRequestDTO.java
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/dto/UpdateCalcInfoRequestDTO.java
@@ -1,5 +1,8 @@
 package br.com.actionlabs.carboncalc.dto;
 
+import br.com.actionlabs.carboncalc.utils.CONSTANTS;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.Data;
 
 import java.util.List;
@@ -10,5 +13,7 @@ public class UpdateCalcInfoRequestDTO {
   private int energyConsumption;
   private List<TransportationDTO> transportation;
   private int solidWasteTotal;
+  @DecimalMin(value = "0.0", message = CONSTANTS.PERCENTAGE_DECIMAL_MIN)
+  @DecimalMax(value = "1.0", message = CONSTANTS.PERCENTEGA_DECIMAL_MAX)
   private double recyclePercentage;
 }
