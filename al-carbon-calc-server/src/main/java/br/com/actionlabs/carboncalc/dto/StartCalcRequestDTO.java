437f8e2b471bbd7568cf1aee599c6d3c04c8bcd3
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-28 21:47:40 -0300

    feat(dto): add annotations 'UniqueEmail' and 'UniquePhoneNumber' for verify exists in database

--- al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/dto/StartCalcRequestDTO.java
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/dto/StartCalcRequestDTO.java
@@ -1,5 +1,7 @@
 package br.com.actionlabs.carboncalc.dto;
 
+import br.com.actionlabs.carboncalc.annotation.UniqueEmail;
+import br.com.actionlabs.carboncalc.annotation.UniquePhoneNumber;
 import br.com.actionlabs.carboncalc.utils.CONSTANTS;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -13,12 +15,14 @@ public class StartCalcRequestDTO {
 
   @NotBlank(message = CONSTANTS.EMAIL_NOT_BLANK)
   @Email(message = CONSTANTS.EMAIL_INVALID)
+  @UniqueEmail
   private String email;
 
   @NotBlank(message = CONSTANTS.UF_NOT_BLANK)
   private String uf;
 
   @NotBlank(message = CONSTANTS.PHONE_NOT_BLANK)
+  @UniquePhoneNumber
   private String phoneNumber;
 
 }
