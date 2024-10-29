dc606f63b606b67cb5ab0ccaff7c5b7931f29921
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-10-28 23:03:02 -0300

    feat(mapper): add method to convert attributes to dto for carbon calculation result mapper class

---
+++ al-carbon-calc-server/src/main/java/br/com/actionlabs/carboncalc/mapper/CarbonCalculationResultMapper.java
@@ -0,0 +1,19 @@
+package br.com.actionlabs.carboncalc.mapper;
+
+import br.com.actionlabs.carboncalc.dto.CarbonCalculationResultDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CarbonCalculationResultMapper {
+
+    default CarbonCalculationResultDTO toDTO(double energy, double transportation, double solidWaste) {
+        final CarbonCalculationResultDTO calculationResultDTO = new CarbonCalculationResultDTO();
+        calculationResultDTO.setEnergy(energy);
+        calculationResultDTO.setSolidWaste(transportation);
+        calculationResultDTO.setTransportation(solidWaste);
+        calculationResultDTO.setTotal(energy + solidWaste + transportation);
+
+        return calculationResultDTO;
+    }
+
+}
